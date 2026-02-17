2b3272254e3367a9d10f5137b4eb828ca12a9ba2
abhi29-tech <abhinandanauk099@gmail.com>
2026-02-17 10:13:17 +0530

    Add files via upload

---
+++ day1(leetpy)/demo.py
@@ -0,0 +1,101 @@
+# apple_fruit=10
+# print(apple_fruit)
+# _apple=10
+# print(_apple)
+
+# datatype
+# 4 datatype: int,float,string,boolian
+#operators
+#a++ --> a=a+1 in other lang
+#a=+1 --> in python
+
+#task1
+# birthyr=2005
+# current_yr=2026
+# print(current_yr-birthyr)
+
+#task2
+# current_age=21
+# max_age=70
+# amt_pd=4
+# a=max_age-current_age
+# b=a*amt_pd
+# print(b)
+
+#task3
+# current_age=21
+# max_age=80
+# perday_cons=4
+# a=max_age-current_age
+# b=a*365
+# c=b*4
+# print(c)
+
+# task4
+# trip_dist=1000
+# milage=20
+# petrol_prize=100
+# a=trip_dist/milage
+# b=a*petrol_prize
+# print(a)
+# print(b)
+
+#task5
+# daily_data_usage=2
+# tot_days=30
+# tot_monthly_data=daily_data_usage*tot_days
+# print(tot_monthly_data)
+
+#task6
+# current_age=20
+# a=current_age*365
+# print(a)
+
+#task7
+# og_prize=5000
+# discount_per=5
+# a=discount_per/100*og_prize
+# b=og_prize-a
+# print(b)
+
+#task8
+# a=100
+# b=15
+# if a<b:
+#     print("a is less than b")
+# else: 
+#     print("a is greater than b")
+
+#task9
+# string=input("enter ch=")
+# if string=="a" or string=="e" or string=="i" or string=="o" or string=="u":
+#     print('vowel')
+# else:
+#     print("consonet")
+
+#task10
+# mark = int(input("Enter the mark = "))
+
+# if mark > 100:
+#     print("Invalid grade")
+# elif mark == 100:
+#     print("Your grade is S")
+# elif 90 <= mark <= 99:
+#     print("Grade is A")
+# elif 80 <= mark <= 89:
+#     print("Grade is B")
+# elif 70 <= mark <= 79:
+#     print("Grade is C")
+# elif 60 <= mark <= 69:
+#     print("Grade is D")
+# elif 50 <= mark <= 59:
+#     print("Grade is E")
+# else:
+#     print("Fail")
+
+
+
+
+
+
+
